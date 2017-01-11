5831f07828d2c0d131cc34023d50b23fcbe8a484
J196W <wilcoxjaca0@gmail.com>
2017-01-11 14:36:59 -0600

    Adding (14)Elevens Lab Pt.2

---
+++ (14)Elevens Lab Pt. 2/src/DeckTester.java
@@ -0,0 +1,27 @@
+
+public class DeckTester {
+	public static void main (String[] args){
+		//Example Test
+		String[] ranksA = {"A", "B", "C"};
+		String[] suitsA = {"Giraffes", "Lions"};
+		int[] valuesA = {2,1,6};
+		Deck d = new Deck(ranksA, suitsA, valuesA);
+		System.out.println(d.size());
+		
+		//Size Test
+		String[] ranksB = {"jack", "queen", "king"};
+		String[] suitsB = {"blue", "red"};
+		int[] valuesB = {11, 12, 13};
+		d = new Deck(ranksB, suitsB, valuesB);
+		System.out.println(d.size());
+		
+		//Twenty-One Test
+		String[] ranksC = {"two", "three", "four", "five", "six", 
+				"seven", "eight", "nine", "ten", "jack", "queen", "king", "ace"};
+		String[] suitsC = {"Hearts", "Spades", "Diamond", "Clubs"};
+		int[] valuesC = {2,3,4,5,6,7,8,9,10,10,10,10,11};
+		d = new Deck(ranksC, suitsC, valuesC);
+		System.out.println(d.size());
+		
+	}
+}
